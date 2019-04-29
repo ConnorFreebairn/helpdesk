936244a1fdf065b6f9979f956b522cf51913e202
Connor Freebairn <connor.freebairn@outlook.com>
2019-04-29 19:23:59 +0100

    Adds authentication page for technicians
    Adds authentication page for technicians

---
+++ Help Desk/authentication.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Help_Desk
+{
+    partial class authentication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exit_button = new System.Windows.Forms.Button();
+            this.login_button = new System.Windows.Forms.Button();
+            this.password_label = new System.Windows.Forms.Label();
+            this.username_label = new System.Windows.Forms.Label();
+            this.password_textBox = new System.Windows.Forms.TextBox();
+            this.username_textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // exit_button
+            // 
+            this.exit_button.Location = new System.Drawing.Point(219, 86);
+            this.exit_button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(112, 31);
+            this.exit_button.TabIndex = 4;
+            this.exit_button.Text = "Exit";
+            this.exit_button.UseVisualStyleBackColor = true;
+            this.exit_button.Click += new System.EventHandler(this.Exit_button_Click);
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(339, 86);
+            this.login_button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(112, 31);
+            this.login_button.TabIndex = 3;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.Login_button_Click);
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Location = new System.Drawing.Point(15, 53);
+            this.password_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(78, 20);
+            this.password_label.TabIndex = 9;
+            this.password_label.Text = "Password";
+            // 
+            // username_label
+            // 
+            this.username_label.AutoSize = true;
+            this.username_label.Location = new System.Drawing.Point(10, 17);
+            this.username_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.username_label.Name = "username_label";
+            this.username_label.Size = new System.Drawing.Size(83, 20);
+            this.username_label.TabIndex = 8;
+            this.username_label.Text = "Username";
+            // 
+            // password_textBox
+            // 
+            this.password_textBox.Location = new System.Drawing.Point(103, 50);
+            this.password_textBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.password_textBox.Name = "password_textBox";
+            this.password_textBox.PasswordChar = '*';
+            this.password_textBox.Size = new System.Drawing.Size(348, 26);
+            this.password_textBox.TabIndex = 2;
+            // 
+            // username_textBox
+            // 
+            this.username_textBox.Location = new System.Drawing.Point(103, 14);
+            this.username_textBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.username_textBox.Name = "username_textBox";
+            this.username_textBox.Size = new System.Drawing.Size(348, 26);
+            this.username_textBox.TabIndex = 1;
+            // 
+            // authentication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 129);
+            this.Controls.Add(this.exit_button);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.username_label);
+            this.Controls.Add(this.password_textBox);
+            this.Controls.Add(this.username_textBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.Name = "authentication";
+            this.ShowIcon = false;
+            this.Text = "Authentication";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.Label username_label;
+        private System.Windows.Forms.TextBox password_textBox;
+        private System.Windows.Forms.TextBox username_textBox;
+    }
+}
+
---
+++ Help Desk/authentication.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Help_Desk
+{
+    public partial class authentication : Form
+    {
+        public authentication()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_button_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=CONNOR-PC\SQLEXPRESS;Initial Catalog=Authentication;Integrated Security=True;Pooling=False");
+            string query = "Select * from Users Where username = '" + username_textBox.Text.Trim() + "' and password = '" + password_textBox.Text.Trim() + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+            if (dtbl.Rows.Count == 1)
+            {
+                dashboard objdashboard = new dashboard();
+                this.Hide();
+                objdashboard.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please check your username and password.");
+            }
+        }
+
+        private void Exit_button_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
