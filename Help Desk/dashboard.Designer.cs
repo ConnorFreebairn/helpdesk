b90d5e3428a079721d4f5430fce964df18637331
Connor Freebairn <connor.freebairn@outlook.com>
2019-04-29 19:55:46 +0100

    Started design on ticket dashboard
    Started design on ticket dashboard

--- Help Desk/dashboard.Designer.cs
+++ Help Desk/dashboard.Designer.cs
@@ -29,11 +29,86 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("All Open Tickets");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("All Closed Incidents");
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.userManagmentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openSelectedTicketToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.userManagmentToolStripMenuItem,
+            this.openSelectedTicketToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1144, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // userManagmentToolStripMenuItem
+            // 
+            this.userManagmentToolStripMenuItem.Name = "userManagmentToolStripMenuItem";
+            this.userManagmentToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
+            this.userManagmentToolStripMenuItem.Text = "User managment";
+            // 
+            // openSelectedTicketToolStripMenuItem
+            // 
+            this.openSelectedTicketToolStripMenuItem.Name = "openSelectedTicketToolStripMenuItem";
+            this.openSelectedTicketToolStripMenuItem.Size = new System.Drawing.Size(126, 20);
+            this.openSelectedTicketToolStripMenuItem.Text = "Open selected ticket";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 27);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "All Open Tickets";
+            treeNode2.Name = "Node1";
+            treeNode2.Text = "All Closed Incidents";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            this.treeView1.Size = new System.Drawing.Size(203, 543);
+            this.treeView1.TabIndex = 1;
+            // 
+            // tableBindingSource
+            // 
+            this.tableBindingSource.DataMember = "Table";
+            // 
+            // authenticationDataSet
+            // 
+            // tableTableAdapter
+            // 
+            // 
+            // dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1144, 582);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "dashboard";
             this.Text = "dashboard";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem userManagmentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openSelectedTicketToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.BindingSource tableBindingSource;
     }
 }
